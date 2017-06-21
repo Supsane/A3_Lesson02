2a411b63bc7aac87a8996c4bc91f0b8935f4e0de
Supsane <echashurin@gmail.com>
2017-06-21 10:19:17 +0300

    add 2 observable

--- app/src/main/java/ru/geekbrains/gviewer/model/InfoModelImpl.java
+++ app/src/main/java/ru/geekbrains/gviewer/model/InfoModelImpl.java
@@ -3,7 +3,9 @@ package ru.geekbrains.gviewer.model;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Func1;
+import rx.functions.Func2;
 
 public class InfoModelImpl implements InfoModel {
 
@@ -13,19 +15,35 @@ public class InfoModelImpl implements InfoModel {
 
     @Override
     public Observable<String> retrieveInfo() {
-        return Observable.timer(2L, TimeUnit.SECONDS).flatMap(new Func1<Long, Observable<String>>() {
+//        return Observable.timer(2L, TimeUnit.SECONDS).flatMap(new Func1<Long, Observable<String>>() {
+//            @Override
+//            public Observable<String> call(Long aLong) {
+//                Observable<String> result;
+//                double random = Math.random();
+//                if (random > 0.5 && random < 0.75) {
+//                    result = Observable.just(FUBAR);
+//                } else if (random > 0.75) {
+//                    result = Observable.just(SUSFU);
+//                } else {
+//                    result = Observable.error(new IllegalStateException(BOHICA));
+//                }
+//                return result;
+//            }
+//        });
+
+        Observable<String> stringObservable = Observable.timer(2L, TimeUnit.SECONDS).flatMap(new Func1<Long, Observable<String>>() {
             @Override
             public Observable<String> call(Long aLong) {
-                Observable<String> result;
-                double random = Math.random();
-                if (random > 0.5 && random < 0.75) {
-                    result = Observable.just(FUBAR);
-                } else if (random > 0.75) {
-                    result = Observable.just(SUSFU);
-                } else {
-                    result = Observable.error(new IllegalStateException(BOHICA));
-                }
-                return result;
+                return Observable.just(FUBAR, SUSFU, FUBAR, SUSFU, FUBAR, SUSFU, FUBAR, SUSFU, FUBAR, SUSFU);
+            }
+        });
+
+        Observable<Integer> integerObservable = Observable.range(1, 10);
+
+        return Observable.combineLatest(stringObservable, integerObservable, new Func2<String, Integer, String>() {
+            @Override
+            public String call(String s, Integer integer) {
+                return s + integer;
             }
         });
     }
